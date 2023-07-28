c803d875640995969d5a74be7d61b0e361833fab
David Riley <Dril112191@gmail.com>
2023-07-28 13:03:51 -0400

    Fix submenus not working properly on smaller screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,31 @@
 import { MyContext } from "../App"
 import { useContext } from 'react'
+import { useState } from 'react'
 
 export default function Header(){
-    const {category} = useContext(MyContext)
+    const {category, setCategory} = useContext(MyContext)
+    const [searchInput, setSearchInput] = useState('');
+
+    function handleSubmit(e: React.ChangeEvent<HTMLFormElement>){
+        e.preventDefault()
+        setCategory(searchInput)
+        console.log(category)
+    }
+
+    function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+        e.preventDefault()
+        setSearchInput(e.target.value);
+    }
+
     return(
         <div className="header-container">
             <h1 className="header-text">Keep Up To Date With The Latest News.</h1>
             <h1 className="text-xl font-semibold italic mt-3">{category.toUpperCase()} NEWS </h1>
+            <form onSubmit={handleSubmit}>
+                <input className="search-bar" type="text" value={searchInput} onChange={handleInput} />
+                <button className="search-btn" type="submit">Search</button>
+            </form>
+            
         </div>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { MyContext } from "../App";
 
@@ -16,6 +16,12 @@ const navItems = [
 export default function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
   const { setCategory, setSubCategory } = useContext(MyContext);
+  const [showSubMenu, setShowSubMenu] = useState(false)
+
+  const closeAllMenusOnBigScreen = () => {
+    setShowMenu(false);
+    setShowSubMenu(false);
+  };
 
   const toggleMenu = () => {
     setShowMenu((prevShowMenu) => !prevShowMenu);
@@ -34,15 +40,31 @@ export default function Navbar() {
     setShowMenu(false); 
   };
 
+  const toggleSubMenu = () =>{
+    setShowSubMenu((prevShowMenu) => !prevShowMenu)
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 700) {
+        closeAllMenusOnBigScreen();
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="navbar-container">
       <ul className={`navbar-elements`}>
         {navItems.map((item) => (
-          <li key={item.name}>
+          <li className={item.subCategories ? "sub-categories" : "li-elements"} key={item.name}>
             {item.subCategories ? (
               <div className="sub-categories">
                 {/* Show the Politics button with the onClick to toggle subcategories */}
-                <span onClick={toggleMenu}>{item.name}</span>
+                <span className="sub-cat" onClick={toggleMenu}>{item.name}</span>
                 {showMenu && (
                   <ul className={`navbar-elements-dropdown `}>
                     {item.subCategories.map((subCategoryItem) => (
@@ -72,18 +94,18 @@ export default function Navbar() {
       </div>
 
       {/* Dropdown menu for smaller screens */}
-      {showMenu && window.innerWidth < 1000 && (
+      {showMenu && window.innerWidth < 700 && (
         <ul className="navbar-elements-dropdown">
           {navItems.map((item) => (
             <li className="li-elements" key={item.name}>
              {item.subCategories ? (
               <div className="sub-categories">
                 {/* Show the Politics button with the onClick to toggle subcategories */}
-                <span onClick={toggleMenu}>{item.name}</span>
-                {showMenu && (
-                  <ul className={`navbar-elements-dropdown `}>
+                <span onClick={toggleSubMenu}>{item.name}</span>
+                {showMenu && showSubMenu &&(
+                  <ul className={`navbar-elements-dropdown sub-list-container`}>
                     {item.subCategories.map((subCategoryItem) => (
-                      <li className="sub-list" key={subCategoryItem}>
+                      <li className="sub-list " key={subCategoryItem}>
                         <Link to={`${item.path}/${subCategoryItem}`} onClick={() => handleSubCategoryClick(subCategoryItem)}>
                           {subCategoryItem}
                         </Link>
