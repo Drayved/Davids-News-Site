f4fbbb1158eef162bf16fe55c96ab4a00d2fce92
David Riley <Dril112191@gmail.com>
2023-07-28 14:43:56 -0400

    Added date and source to cards.

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -5,27 +5,49 @@ interface NewsItem {
   description: string;
   url: string;
   urlToImage: string;
+  publishedAt: string;
+  source: {
+    id: string,
+    name: string
+  }
   // Add more properties as needed
 }
 
-export default function NewsCard({ newsItem }: { newsItem: NewsItem }) {
-  
-  
-    if (!newsItem.urlToImage) {
-        return null; // Return null to skip rendering the card
-      }
-      
+export default function NewsCard({ newsItem }: { newsItem: NewsItem}) {
+  const formatPublishedAt = (publishedAt: string) => {
+    const date = new Date(publishedAt);
+    const options = {
+      timeZone: "America/New_York", // Set the time zone to American Eastern Standard Time (EST)
+      year: "numeric" as const,
+      month: "long" as const,
+      day: "numeric" as const,
+      hour: "numeric" as const,
+      minute: "numeric" as const
+    };
+    return date.toLocaleString("en-US", options);
+  };
+
+  if (!newsItem.urlToImage) {
+    return null; // Return null to skip rendering the card
+  }
+
   return (
     <div className="news-cards">
-        <div className="news-image">
-            <img src={newsItem.urlToImage} alt={newsItem.title} />
+      <div className="news-image">
+        <img src={newsItem.urlToImage} alt={newsItem.title} />
+      </div>
+      <div className="news-details">
+        <div className="article-info-container">
+          <p>{formatPublishedAt(newsItem.publishedAt)}</p>
+          <p>{newsItem.source.name}</p>
         </div>
-        <div className="news-details">
-            <h3 className="news-title">{newsItem.title}</h3>
-            <p className="news-description">{newsItem.description}</p>
-            <a className="read-more" href={newsItem.url} target="_blank" rel="noopener noreferrer">
-            Read more
-            </a>
+        
+        <h3 className="news-title">{newsItem.title}</h3>
+        
+        <p className="news-description">{newsItem.description}</p>
+        <a className="read-more" href={newsItem.url} target="_blank" rel="noopener noreferrer">
+          Read more
+        </a>
       </div>
     </div>
   );
